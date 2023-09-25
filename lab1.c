57c0bd160484b23d5e612af0d068012fefba6418
Yoriichi2222 <vs973222@gmail.com>
2023-09-26 00:02:32 +0500

    Ура код
    ))))

---
+++ lab1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void)
+{
+    int b1, b2, b3;
+    float x1, x2, x3;
+    printf("Enter b1 -> ");
+    scanf("%d", &b1);
+    printf("Enter b2 -> ");
+    scanf("%d", &b2);
+    printf("Enter b3 -> ");
+    scanf("%d", &b3);
+    x1 = b1 / 1;
+    x2 = b2 / 2;
+    x3 = b3 / 3;
+    
+    if(b1 & 1 && b1 < b2 &&  b1 < b3){
+        printf("наименьший параметр 1\n"); 
+    }
+            else{
+        printf("%.1f\n", x1);
+    }
+
+
+     if(b2 & 1 && b2 < b1 &&  b2 < b3){
+        printf("наименьший параметр 2\n"); 
+    }
+            else{
+        printf("%.2f\n", x2);
+    }
+
+    if(b3 & 1 && b3 < b1 &&  b3 < b2){
+        printf("наименьший параметр 3\n"); 
+    }
+            else{
+        printf("%.3f\n", x3);
+    }
+    return 0;
+}
